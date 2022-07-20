df0f5b1d5c47d810fcbb68825cbce166b6bdfea8
Dwi <1472909+luneareth@users.noreply.github.com>
2022-07-20 13:03:39 +0700

    Update sui.sh

--- sui.sh
+++ sui.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
+build="devnet"
+tags="0.6.1"
+version="${build}-${tags}"
+blob_url="https://github.com/MystenLabs/sui-genesis/blob/tharbert/${version}/devnet/genesis.blob?raw=true"
+docker_compose=file
+
 if ! [ -f docker-compose.yaml ]
 then
+if [ ${docker_compose} == "file" ]
+then
+cat > docker-compose.yaml <<EOF
+---
+version: "3.9"
+services:
+  fullnode:
+    container_name: sui-validator
+    image:  mysten/sui-node:stable
+    ports:
+    - "9000:9000"
+    - "9184:9184"
+    expose:
+    - "9000"
+    - "9184"
+    volumes:
+    - ./fullnode.yaml:/sui/fullnode.yaml:ro
+    - ./genesis.blob:/sui/genesis.blob:ro
+    - suidb:/sui/suidb:rw
+    command: ["/usr/local/bin/sui-node", "--config-path", "fullnode.yaml"]
+volumes:
+  suidb:
+EOF
+else
 wget https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
+fi
 sed -i 's/fullnode-template.yaml/fullnode.yaml/' docker-compose.yaml
 fi
 if ! [ -f fullnode.yaml ]
@@ -13,9 +44,9 @@ sed -i 's/127.0.0.1:9000/0.0.0.0:9000/' fullnode.yaml
 fi
 if ! [ -f genesis.blob ]
 then
-wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+wget ${blob_url}
 fi
-check_network=`docker network ls | grep sui-network`
+check_network=`docker network ls | grep sui-network | wc -l`
 if [ ${check_network} -neq 0 ]
 then
   docker network rm sui-network
@@ -24,6 +55,6 @@ else
   docker network create sui-network
 fi
 docker-compose up -d
-container_id=`docker ps | grep sui |  awk '{print $1}i'`
-docker update --restart=unless-stopped ${container_id}
+container_name=`docker ps | grep sui |  awk '{print $1}i'`
+docker update --restart=unless-stopped ${container_name}
 
